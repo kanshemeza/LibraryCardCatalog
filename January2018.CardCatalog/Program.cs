bf2b2d690d0bf93722dd6385a924b69e92617755
Joe Johnson <joe.wendell.johnson@gmail.com>
2018-01-15 10:22:05 -0600

    Add project files.

---
+++ January2018.CardCatalog/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace January2018.CardCatalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
